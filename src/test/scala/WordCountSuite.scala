7cfd73e78d63c2e4fdaedbd5facbbd1673f4c40f
Zhao Zhang <zzhang@wireless-10-147-67-252.public.utexas.edu>
2017-04-24 16:55:01 -0500

    update wordcount test case

--- src/test/scala/WordCountSuite.scala
+++ src/test/scala/WordCountSuite.scala
@@ -1,8 +1,12 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkContext
 import org.scalatest.FunSuite
 
 class WordCountSuite extends FunSuite{
   test("WordCount Test"){
-    val s = "hello, world!"
-    assert(s == "hello, world!")
+  	val sc = new SparkContext("local", "test")
+    val rdd = sc.parallelize(List(1,2,3,4))
+    val res = rdd.reduce(_+_)
+    assert(res == 10)
   }
 }
