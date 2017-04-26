5bada07d92f412fa738d52834c09f89f5e7d8003
Zhao Zhang <zzhang@wireless-10-147-67-252.public.utexas.edu>
2017-04-26 17:43:30 -0500

    update unit test case

--- src/test/scala/WordCountSuite.scala
+++ src/test/scala/WordCountSuite.scala
@@ -6,7 +6,7 @@ class WordCountSuite extends FunSuite{
   test("WordCount Test"){
   	val sc = new SparkContext("local", "test")
     val rdd = sc.parallelize(List(1,2,3,4))
-    val res = rdd.reduce(_+_)
-    assert(res == 10)
+    val res = rdd.map(WordCount.func(_))
+    assert(res.collect.sameElements(Array(1,4,9,16)))
   }
 }
