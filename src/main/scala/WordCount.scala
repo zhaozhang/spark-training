de6ad161a20c3b8dfe28ff5725df820c876d2cf6
zhaozhang <zhangzhao0718@gmail.com>
2017-04-27 11:13:04 -0500

    change path

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -4,10 +4,11 @@ import org.apache.spark.SparkConf
 
 object WordCount {
   def main(args: Array[String]) {
-    val logFile = "/Users/zzhang/Works/spark/spark-1.6.1/conf" // Should be some file on your system
+    //val logFile = "/Users/zzhang/Works/spark/spark-1.6.1/conf" // Should be some file on your system
+    val logFile = "/tmp/data/book.txt" // Should be some file on your system
     val conf = new SparkConf().setAppName("WordCount")
     val sc = new SparkContext(conf)
-    val lines = sc.textFile(logFile, 2).cache()
+    val lines = sc.textFile(logFile).cache()
     val words = lines.flatMap(l => l.split(" "))
     val kwp = words.map(w => (w, 1))
     val res = kwp.reduceByKey(_ + _)
